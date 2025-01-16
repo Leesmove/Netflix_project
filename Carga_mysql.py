5d1ca2a78ca64d94ed1897fcfd565289a1faa4fc
EsperanzaM <97237712+Leesmove@users.noreply.github.com>
2025-01-16 17:23:23 -0500

    Correccion codigo para cargar datos a MySQL

---
+++ Carga_mysql.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Ruta del archivo CSV
+file_path = r"C:\Users\esper\Desktop\Docs_Esperanza\github\Proj_data_analysis\03_Netflix\Netflix_project\Netflix_cleaned.csv"
+
+# Cargar los datos desde el archivo CSV
+df = pd.read_csv(file_path)
+
+# Crear la conexión a MySQL
+engine = create_engine('mysql+mysqlconnector://root:Hopemove07*@localhost/netflix')
+
+# Cargar el DataFrame a la tabla MySQL
+df.to_sql('netflix', con=engine, if_exists='append', index=False)
+
+print("Datos cargados exitosamente a MySQL.")
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\esper\Desktop\Docs_Esperanza\github\Proj_data_analysis\03_Netflix\venv
+set VIRTUAL_ENV=C:\Users\esper\Desktop\Docs_Esperanza\github\Proj_data_analysis\03_Netflix\Netflix_project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
